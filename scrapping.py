8ff04437f4aad7bcace4a48a3727f4abc1fbe769
JonnHenry <jonnathanca9601@gmail.com>
2021-04-25 02:09:48 -0500

    Service to get data from hotels ready to integrated to the main app

--- scrapping.py
+++ scrapping.py
@@ -5,12 +5,15 @@ from typing import List
 from urllib.parse import unquote
 import fake_useragent
 from threading import Thread
+from wikipedia import summary,set_lang
+from time import sleep
 
 location = './fake_useragent%s.json' % fake_useragent.VERSION
 ua = fake_useragent.UserAgent(path=location)
 
 images_urls = []
 wiki_info = ""
+set_lang("es")
 
 def get_images(city:str)-> List[str]:
     global images_urls
@@ -22,9 +25,9 @@ def get_images(city:str)-> List[str]:
     browser.open("https://images.google.com/") #Open link to the google Images
     #target the search input
     browser.select_form() #The form used is 'q'
-    
     #search for a term
     browser["q"] = '{} Ecuador'.format(city) 
+    sleep(2)
     #submit/"click" search
     browser.submit_selected(btnName="btnG")
     
@@ -46,33 +49,11 @@ def get_images(city:str)-> List[str]:
 
 def get_data_wiki(city:str)-> str:
     global wiki_info
-    browser =StatefulBrowser(
-        soup_config={'features': 'lxml'},  # Use the lxml HTML parser
-        raise_on_404=True,
-        user_agent=ua.random
-    )
-    browser.open("https://www.google.com/") #Open link to the google Images
-    #target the search input
-    browser.select_form('form[action="/search"]') #The form used is 'q'
-    #search for a term
-    browser["q"] = "{} Ecuador wikipedia".format(city)
-    #submit/"click" search
-    browser.submit_selected(btnName="btnG")
-    url:str = ""
-    for link in browser.links():
-        target = link.attrs['href']
-        # Filter-out unrelated links and extract actual URL from Google's
-        # click-tracking.
-        if (target.startswith('/url?') and not target.startswith("/url?q=http://webcache.googleusercontent.com")):
-            target = sub(r"^/url\?q=([^&]*)&.*", r"\1", target)
-            url = unquote(target)
-            break 
-    
-    browser.open(url)
-    data_without_clean = browser.get_current_page().find('p').text
+    query = "{} Ecuador ".format(city)
+    data_without_clean = summary(query,sentences=2)
     text_cleaned = sub(r'\s+', ' ',data_without_clean)
     text_cleaned =sub(r'\[.*?\]', '', text_cleaned)
-    wiki_info = sub("\n", '', text_cleaned)
+    wiki_info = sub("\n", ' ', text_cleaned)
 
 
 def get_info(city:str):
@@ -86,3 +67,6 @@ def get_info(city:str):
     thread_wiki.join()
 
     return {"wiki": wiki_info,"imgs": images_urls}
+
+#Testing
+#print(get_info("Cuenca"))
